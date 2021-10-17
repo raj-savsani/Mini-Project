ebfd0d35fd3412d94714e1ac3c67f07d4198092f
raj-savsani <91535449+raj-savsani@users.noreply.github.com>
2021-10-17 21:12:26 +0530

    implemented Signup- login

--- movie/script.js
+++ movie/script.js
@@ -1,6 +1,5 @@
 async function searchMovie(movie_name) {
-    let res = await fetch(`
-https://api.themoviedb.org/3/search/movie?api_key=47d425944b3d5114339b9ddd5176a122&language=en-US&query=${movie_name}&page=1`);
+    let res = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=47d425944b3d5114339b9ddd5176a122&language=en-US&query=${movie_name}&page=1`);
 
     let data = await res.json();
     // console.log('data:', data)
@@ -102,7 +101,6 @@ https://api.themoviedb.org/3/search/movie?api_key=47d425944b3d5114339b9ddd5176a1
     let data = await res.json();
 
     appendPopular(data.results);
-    console.log('data.results:', data.results)
   }
 
   let popular_movies = document.getElementById("popular-movies");
@@ -134,3 +132,150 @@ https://api.themoviedb.org/3/search/movie?api_key=47d425944b3d5114339b9ddd5176a1
     });
   }
   popularmovies();
+
+let login_register = document.getElementById("login-register");
+login_register.addEventListener("click",()=>{
+  document.querySelector(".modal").style.display = "flex";
+})
+
+function cl(){
+  document.querySelector(".modal").style.display = "none";
+}
+
+function reg(){
+  document.querySelector(".reg-div").style.display = "block";
+ document.querySelector(".log-div").style.display = "None";
+ document.getElementById("register-btn").style.backgroundColor = "rgb(107, 83, 83)"
+ document.getElementById("register-btn").style.color = "blanchedalmond";
+ document.getElementById("login-btn").style.backgroundColor = "rgb(240, 240, 240)"
+  document.getElementById("login-btn").style.color = "black";
+}
+function log(){
+  document.querySelector(".log-div").style.display = "block";
+  document.querySelector(".reg-div").style.display = "none";
+  document.getElementById("login-btn").style.backgroundColor = "rgb(107, 83, 83)"
+  document.getElementById("login-btn").style.color = "blanchedalmond";
+  document.getElementById("register-btn").style.backgroundColor = "rgb(240, 240, 240)"
+  document.getElementById("register-btn").style.color = "black";
+}
+
+
+
+function signup(e){
+  e.preventDefault();
+  
+  let form = document.getElementById("signup-form");
+
+  let user_data={
+    name:form.name.value,
+    email:form.email.value,
+    password:form.password.value,
+    username: form.username.value,
+    mobile: form.mobile.value,
+    description: form.description.value,
+  };
+
+  user_data = JSON.stringify(user_data);
+
+  fetch('https://masai-api-mocker.herokuapp.com/auth/register',{
+    method:"POST",
+    body:user_data,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+
+  .then((res)=>{
+    return res.json();
+  })
+
+  .then((res)=>{
+    let p = document.getElementById("reg-msg");
+    let msg = res.message;
+    let err = res.error;
+    if(err == false){
+      p.textContent = msg;
+      p.style.color = "green";
+    }else{
+      p.textContent = msg;
+      p.style.color = "red";
+    }
+    console.log('res:', res)
+  })
+  .catch((err)=>{
+    let p = document.getElementById("reg-msg");
+    let msg = err.message;
+    p.textContent = msg;
+    p.style.color = "red";
+  console.log('err:', err)
+  })
+}
+
+function login(e){
+  e.preventDefault();
+
+  let form = document.getElementById("login-form");
+  
+  let user_data = {
+    username: form.user.value,
+    password: form.pass.value,
+  }
+
+  let data_to_send = JSON.stringify(user_data);
+
+  fetch('https://masai-api-mocker.herokuapp.com/auth/login',{
+    method:"POST",
+    body:data_to_send,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+  .then((res)=>{
+    return res.json();
+  })
+  .then((res)=>{
+    let p = document.getElementById("log-msg");
+    if(res.error == false){
+      p.textContent = "Login Successful";
+      p.style.color = "green";
+      setTimeout(cl,1500);
+    }else{
+      p.textContent = res.message;
+      p.style.color = "red";
+    }
+    console.log('res:', res)
+    fetchMydata(user_data.username,res.token);
+  })
+  .catch((err)=>{
+    let p = document.getElementById("log-msg");
+    let msg = err.message;
+    p.textContent = msg;
+    p.style.color = "red";
+    console.log("e",err);
+  })
+}
+
+function fetchMydata(username,token){
+  fetch(`https://masai-api-mocker.herokuapp.com/user/${username}`,{
+    headers: {
+      "Content-Type": "application/json",
+
+      Authorization: `Bearer ${token}`
+    },
+  })
+  .then((res)=>{
+    return res.json();
+  })
+  .then((res)=>{
+    let replace = document.getElementById("login-register");
+    let name = res.name;
+    if(res.name){
+      replace.textContent = res.name;
+      replace.style.backgroundColor = "#FFC107";
+    }
+    console.log('res:', res)
+  })
+  .catch((err)=>{
+    console.log('err:', err)
+  })
+}
